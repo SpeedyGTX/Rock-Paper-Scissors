06f62b3ab5492df895f868f4e1744d219060c1be
SpeedyGTX <alienarmedgtx@gmail.com>
2022-05-16 17:19:52 +0300

    stuff

--- js-file.js
+++ js-file.js
@@ -1,13 +1,12 @@
 let userScore = 0; /* Initialize user score in order to keep track. */
 let compScore = 0; /* Initialize computer score in order to keep track. */
-let i = 0; /* Variable that's incremented in the playRound function in order for the while loop to not count the rounds with an invalid input */
 
 function computerPlay (){
     const myArray = ["rock", "paper", "scissors"];
     return myArray[Math.floor(Math.random() * myArray.length)]; /* Multiply a float between 0 and 1 by the length of myArray and then round down to the closest integer. The integer within [] represents the array's index. */
 }
 
-/* The playRound function takes two parameters; userSelection and computerSelection. User selection parameter is lowercased to accommodate the input's style of capitalization. If the computer's selection happens to equal to user's input, then the console will output a draw message. Else if the computer's selection beats user's selection in all 3 varieties, a losing round message will be outputted and both i and compScore incremented. If the user's selection beats computer's selection in all 3 varities, a winning round message will be outputted and both i and userScore incremented. Otherwise, an invalid message will be outputted.*/
+/* The playRound function takes two parameters; userSelection and computerSelection. If the computer's selection happens to equal to user's input, then the console will output a draw message. Else if the computer's selection beats user's selection in either of the 3 varieties, a losing round message will be outputted and compScore incremented. If the user's selection beats computer's selection in either of the 3 varities, a winning round message will be outputted and userScore incremented.*/
 function playRound (userSelection, computerSelection){
     userSelection = userSelection.toLowerCase();
     if (computerSelection === userSelection){
@@ -28,25 +27,23 @@ function playRound (userSelection, computerSelection){
         return str.charAt(0).toUpperCase() + str.slice(1); /* charAt picks the first letter of a string and capitalizes it. slice cuts and returns a string from the chosen index. Both strings are then added and returned. */
     }
 
-    /* While i is less than 5, prompt the user to input the selection and then call on the playRound function. Afterwards, compare the scores. That which is higher wins, otherwise it's a draw. */
     function game(){
-            function btnchk(e){
-                let playerSelection = e.target.innerHTML;
-                playRound(playerSelection, computerPlay());
-                i += 1;
-                displayScore();
-            }
-            
+
             const againButton = document.querySelector('#again')
             againButton.style.display = "none";
             document.getElementById("score").style.display = "none";
             const btns = document.querySelectorAll('.button')
             btns.forEach(btn => btn.style.display = "none");
-            const startButton = document.querySelector('#start')
-            startButton.addEventListener('click', () => {
-                fadeEffect();
-                
+
+            document.getElementById('start').addEventListener('click', () => {
+                startGame();
             })
+
+            function btnchk(e){
+                let playerSelection = e.target.innerHTML;
+                playRound(playerSelection, computerPlay());
+                displayScore();
+            }
 
             btns.forEach(btn => btn.addEventListener('click', (e) => {
                 btnchk(e);
@@ -59,39 +56,58 @@ function playRound (userSelection, computerSelection){
             }
 
             const scoreCheck = function(){
-                if(i === 5){
+                if(compScore === 5 || userScore === 5){
+                    btns.forEach(btn => btn.disabled = true);
                     if (userScore > compScore){
-                        document.getElementById("notify").innerHTML = 'You win!';
                         playAgain();
+                        setTimeout(function(){
+                            document.getElementById("notify").innerHTML = 'You win!';
+                        }, 800)
                         
                     } else if (userScore < compScore){
-                        document.getElementById("notify").innerHTML = 'Computer wins!';
                         playAgain();
-                        
+                        setTimeout(function(){
+                            document.getElementById("notify").innerHTML = 'Computer wins!';
+                        }, 800)
+                         
                     } else {
-                        document.getElementById("notify").innerHTML = 'Tie game.';
                         playAgain();
+                        setTimeout(function(){
+                            document.getElementById("notify").innerHTML = 'Tie game.';
+                        }, 800)
                         
                     }
                 }
             }
 
             const playAgain = function(){
-                btns.forEach(btn => btn.style.display = "none");
-                againButton.style.display = "block";
-                againButton.addEventListener('click', () => {
-                    document.getElementById("notify").innerHTML = '';
-                    i = 0;
-                    userScore = 0;
-                    compScore = 0;
-                    displayScore();
-                    againButton.style.display = "none";
-                    btns.forEach(btn => btn.style.display = "inline-block");
-                    
-                })
+                const fadeTarget = document.querySelector('#container');
+                setTimeout(function(){
+                    fadeTarget.classList.toggle('fade');
+                    setTimeout(function(){
+                        btns.forEach(btn => btn.style.display = "none");
+                        againButton.style.display = "block";
+                        fadeTarget.classList.toggle('fade');
+                        againButton.addEventListener('click', () => {
+                            fadeTarget.classList.toggle('fade');
+                            setTimeout(function(){
+                                btns.forEach(btn => btn.disabled = false);
+                                document.getElementById("notify").innerHTML = '<br>';
+                                i = 0;
+                                userScore = 0;
+                                compScore = 0;
+                                displayScore();
+                                againButton.style.display = "none";
+                                btns.forEach(btn => btn.style.display = "inline-block");
+                                fadeTarget.classList.toggle('fade');
+                            }, 400)  
+                        })
+                    },400)
+                    },500)
+                
             }
 
-            const fadeEffect = function(){
+            const startGame = function(){
                 const fadeTarget = document.querySelector('#container');
                 fadeTarget.classList.toggle('fade');
                 setTimeout(function() {
@@ -99,17 +115,8 @@ function playRound (userSelection, computerSelection){
                     btns.forEach(btn => btn.style.display = "inline-block");
                     document.getElementById("score").style.display = "flex";
                     fadeTarget.classList.toggle('fade');
-                }, 500)
-
-                
-                
-
+                }, 400)
             }
-
-
-
-            
-        
     }
 
     game();
